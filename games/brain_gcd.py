7a150c025184a468df6b7ded46b37b4f1c2420a7
Danil63 <danilsss.ct@gmail.com>
2026-03-22 17:58:18 +0300

    min fix

--- games/brain_gcd.py
+++ games/brain_gcd.py
@@ -7,7 +7,8 @@ def brain_gcd():
     first_term = random.randint(1, 100)
     second_term = random.randint(1, 100)
 
-    numbers = print(f'{first_term} {second_term}')
+    print(f'Question: {first_term} {second_term}')
+    numbers = f'{first_term} {second_term}'
     question = gcd(first_term, second_term)
     
     return numbers, str(question)
